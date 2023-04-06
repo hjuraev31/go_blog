9c2a0e487478d52063ce5169e7ba8476ed5ca055
Khusniddin <hjuraev31@gmail.com>
2023-04-06 21:48:18 +0500

    feat: put, delete

--- controller/postController.go
+++ controller/postController.go
@@ -1,13 +1,17 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"math"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/hjuraev31/blog_go/database"
 	"github.com/hjuraev31/blog_go/models"
+	"github.com/hjuraev31/blog_go/util"
+	"gorm.io/gorm"
 )
 
 func CreatePost(c *fiber.Ctx) error {
@@ -46,4 +50,53 @@ func AllPost(c *fiber.Ctx) error {
 		},
 	})
 
+}
+
+func GetPostDetail(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var blogpost models.Blog
+	database.DB.Where("id=?", id).Preload("User").First(&blogpost)
+	return c.JSON(fiber.Map{
+		"data": blogpost,
+	})
+}
+
+func UpdatePost(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	blog := models.Blog{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&blog); err != nil {
+		log.Println("Unable to parse the body")
+	}
+	database.DB.Model(&blog).Updates(blog)
+	return c.JSON(fiber.Map{
+		"message": "Post updated successfully",
+	})
+}
+
+func UniquePost(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	id, _ := util.Parsejwt(cookie)
+	var blog []models.Blog
+	database.DB.Model(&blog).Where("user_id=?", id).Preload("User").Find(&blog)
+	return c.JSON(blog)
+}
+
+func DeletePost(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	blog := models.Blog{
+		Id: uint(id),
+	}
+	deleteQuery := database.DB.Delete(&blog)
+	if errors.Is(deleteQuery.Error, gorm.ErrRecordNotFound) {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Oop! record not found",
+		})
+	}
+	return c.JSON(fiber.Map{
+		"message": "Record deleted successfully!",
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,16 +1,33 @@
 package routes
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/hjuraev31/blog_go/controller"
 	"github.com/hjuraev31/blog_go/middleware"
+	"github.com/hjuraev31/blog_go/models"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
 
+	app.Get("/api/test/:param", func(c *fiber.Ctx) error {
+		var blog models.Blog
+		if err := c.BodyParser(&blog); err != nil {
+			fmt.Println("error while parsing body")
+		}
+		fmt.Println(blog.Title)
+		c.Append("Link", "http://google.com", "http://localhost")
+		return c.SendString("param: " + c.Params("param"))
+	})
+
 	app.Use(middleware.IsAuthenticated)
 	app.Post("/api/post", controller.CreatePost)
 	app.Get("/api/allposts", controller.AllPost)
+	app.Get("/api/get_post_detail/:id", controller.GetPostDetail)
+	app.Put("/api/update_post/:id", controller.UpdatePost)
+	app.Get("/api/unique_post", controller.UniquePost)
+	app.Get("/api/delete_post/:id", controller.DeletePost)
 }
